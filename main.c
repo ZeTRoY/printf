8683b9116c4e4237e2de7f3c9518b297cf3a119f
Anton ROI <aroi@e2r6p2.unit.ua>
2018-05-19 15:50:38 +0300

    [Upd] - 3%

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aroi <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/19 15:18:48 by aroi              #+#    #+#             */
+/*   Updated: 2018/05/19 15:50:19 by aroi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -16,14 +28,11 @@ int		ft_count_qnt(char *str)
 	return (qnt);
 }
 
-void	ft_whatisit(char *tmp, char c)
+void	ft_whatisit(char *str)
 {
 	int i;
 
 	i = 0;
-	if (*(tmp + 1) == c)
-	{
-	}
 }
 
 int		ft_printf(char *str, ...)
@@ -34,22 +43,19 @@ int		ft_printf(char *str, ...)
 	va_list argPointer;
 	int qnt;
 	
-	i = 0;
+	i = -1;
 	qnt = ft_count_qnt(str);
 	va_start(argPointer, qnt);
-	while (qnt-- > 0)
+	while (++i < qnt)
 	{
 		tmp = ft_strchr(str, '%');
-		c = va_arg(argPointer, char);
-		ft_whatisit(tmp, c);
-		if (*(tmp + 1) == '%')
-			str = tmp + 2;
-		else
-			str = tmp + 1;
+		ft_putstr(str - tmp);
+		str = tmp + 1;
+		ft_whatis(str);
 	}
 	ft_putstr(tmp);
 	va_end(argPointer);
-	return (0);
+	return (i);
 }
 
 int		main(int argc, char **argv)
